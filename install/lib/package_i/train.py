6b04aba8a27650649776126e9e0eac34d80fd73d
uch428 <uchiii428@gmail.com>
2020-03-09 18:46:20 +0100

    comment

--- install/lib/package_i/train.py
+++ install/lib/package_i/train.py
@@ -4,39 +4,60 @@ import numpy as np
 import pandas as pd
 import csv
 
-VoltageData = pd.read_csv('/home/yuta/ros/workspaces/myWorkspace/voltageData.csv', headers=None)
-WeightData = pd.read_csv('/home/yuta/ros/workspaces/myWorkspace/scaleExample.csv', headers=None)
 
-print('---')
+
+
+#parepare np array
+
+numArr = np.array
+volArr = np.array
+weightArr = np.array
+
+# insert data from csv
+Data = pd.read_csv('/home/yuta/ros/workspaces/myWorkspace/calibration.csv', header = None)
+
 
 def calculate():
 
-	# put data from csv into  numpy matrix
-	array_V = np.array
-	array_F = np.array
 
-	array_V = VoltageData.values
-	array_F = WeightData.values
+	dataArr = Data.values
+	print(dataArr)
 
-	#array_F_t = array_F.T
-	
+	# split them into index, data from sensors, data from scale
+	numArr, volArr = np.hsplit(dataArr, [1])
+	volArr, weightArr = np.hsplit(volArr, [8])
+	print(numArr)
+	print'================'
+	print(volArr)
+	print'================'
+	print(weightArr)
+
+	print('=======')
+
+
 	# calculate Pseudo-inverse Matrix of V
-	pinv_V = np.linalg.pinv(array_V)	
-	print(pinv_V)
+		#pinv_V = np.linalg.pinv(array_V)	
+	pinv_volArr = np.linalg.pinv(volArr)	
+	print(pinv_volArr)
 
 	with open("pinv.csv", "w") as f_pinv:
-		csv_w_p = csv.writer(f_pinv)
-		csv_w_p.writerows(pinv_V)
+			#csv_w_p = csv.writer(f_pinv)
+			#csv_w_p.writerows(pinv_volArr)
+		writerPinv = csv.writer(f_pinv)
+		writerPinv.writerows(pinv_volArr)		
+		
 
 	# multiple
-#	a = pinv_V * array_F
-	a = np.dot(pinv_V, array_F)
-	print(a)
+	#result = pinv_V * array_F
+	result = np.dot(pinv_volArr, weightArr)
+	print(result)
 
 	with open("/home/yuta/ros/workspaces/myWorkspace/parameters.csv", 'w') as f_result:
-		csv_w = csv.writer(f_result)
-		csv_w.writerows(a)
-
+			#csv_w = csv.writer(f_result)
+			#csv_w.writerows(a)
+		writerResult = csv.writer(f_result)
+		writerResult.writerows(result)
+		
 
 if __name__ == '__main__':
 
