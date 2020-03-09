c2b19053944f555e8a1f8d246707bcba5453eee9
uch428 <uchiii428@gmail.com>
2020-03-09 18:06:39 +0100

    add new file

---
+++ my_nodeX_i.cpp
@@ -0,0 +1,14 @@
+#include "ros/ros.h"
+int main(int argc, char **argv)
+{
+	ros::init(argc, argv, "myNodeName", ros::init_options::AnonymousName);
+	ros::NodeHandle n;
+	
+	ros::Rate loop_rate(1~);
+	while (ros::ok())
+	{
+		ros::spinOnce();
+		loop_rate.sleep();
+	}
+	return 0;
+}
